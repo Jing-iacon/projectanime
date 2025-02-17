430e4b56b44268909108db24b81dc214d71ca301
Jing-iacon <lapin.katemake@gmail.com>
2025-02-18 00:47:43 +0700

    homeLoader

--- src/pages/Home/homeLoader.ts
+++ src/pages/Home/homeLoader.ts
@@ -5,8 +5,8 @@ import { getAnimeFeature } from "../../api/queries/getAnimeFeature";
 export async function homeLoader() {
   // จะมี Obj เข้ามาใน loader เเล้วจะมี props ที่ขื่อ Request ดึงตัวนี้ออกมา
 
-  const topAnime = await getAnimeFeature(); // ดึง searchResult มาใช้หรือเรียก api นั่นล่ะ
-//   const seasonAnime = await getAnimeSeason(); // ดึง searchResult มาใช้หรือเรียก api นั่นล่ะ
+  const topAnime = await getAnimeFeature(); // ดึง getAnimeFeature มาใช้หรือเรียก api นั่นล่ะ
+  
   return {
     topAnime,
     // seasonAnime,
