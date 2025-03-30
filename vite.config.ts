60b4a56df8e3abbd7cb2b4eb05981c46923a270c
Jing-iacon <lapin.katemake@gmail.com>
2025-03-31 00:04:09 +0700

    Remove GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(),react()],
-  base: "/Animeweb/",
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [tailwindcss(),react()],
+//   base: "/projectanime/"
   
+// })
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [tailwindcss(),react()],
+    base: '/projectanime/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
 })
